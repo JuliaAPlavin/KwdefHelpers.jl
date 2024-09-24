a38e739c7351696c1110aff72c3aee23f6f9bf27
Alexander Plavin <alexander@plav.in>
2024-09-24 00:00:00 +0000

    implementation

--- src/KwdefHelpers.jl
+++ src/KwdefHelpers.jl
@@ -1,3 +1,49 @@
 module KwdefHelpers
 
+using AccessorsExtra
+
+export kwdef_argvals
+
+struct Throws{E}
+    e::E
+end
+
+_eval(code, x::Core.SSAValue) = _eval(code, code[x.id])
+_eval(code, x::GlobalRef) = eval(x)
+_eval(code, x) = x
+
+function _eval(code, x::Core.SlotNumber)
+    assignment = filter(c -> Base.isexpr(c, :(=)) && c.args[1] == x, code) |> only
+    _eval(code, assignment.args[2])
+end
+
+function _eval(code, x::Expr)
+    x = @modify(x.args[∗] |> If(a -> !(a isa GlobalRef))) do v
+        _eval(code, v)
+    end
+    uke = filter(a -> a isa UndefKeywordError, x.args)
+    length(uke) == 1 ? Throws(only(uke)) : eval(x)
+end
+
+function kwdef_argvals(::Type{T}; kwargs...) where {T}
+    m = first(methods(T))
+    kwnames = Base.kwarg_decl(m) |> Tuple
+
+    ci = code_lowered(T)[1]
+    @assert ci.code[end] isa Core.ReturnNode
+    constructor = ci.code[end-1]
+    @assert Base.isexpr(constructor, :call)
+    kwargslots = constructor.args[2:end-1]
+
+    vals = map(sl -> _eval(ci.code, sl), kwargslots) |> Tuple
+    vals = @modify(vals |> RecursiveOfType(Throws{UndefKeywordError})) do t
+        get(kwargs, t.e.var, t)
+    end
+    kws = NamedTuple{kwnames}(vals)
+    _filter(v -> isempty(getall(v, RecursiveOfType(Throws{UndefKeywordError}))), kws)
+end
+
+# XXX: remove when released in Julia
+_filter(f, xs::NamedTuple) = xs[filter(k -> f(xs[k]), keys(xs))]
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,20 @@ using TestItemRunner
 @run_package_tests
 
 
+@testitem "usage" begin
+    f() = 456
+    @kwdef struct MyS{T}
+        somefield = 123
+        another::Union{Int,Nothing} = nothing
+        somemore
+        lastone::Vector{T} = [1+2im, somemore, f()+4im, somefield]
+    end
+
+    @test kwdef_argvals(MyS) == (somefield = 123, another = nothing)
+    @test kwdef_argvals(MyS; somemore=567) == (somefield = 123, another = nothing, somemore = 567, lastone = Number[1 + 2im, 567, 456 + 4im, 123])
+end
+
+
 @testitem "_" begin
     import Aqua
     Aqua.test_all(KwdefHelpers; ambiguities=false)
